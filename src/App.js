be38b981a7374c969bed7fbd280fa6d992614aae
KELLY LUU <kellyluuu@gmail.com>
2022-06-08 23:57:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import Header from './review/Header';
+import ProductReviewList from './review/ProductReviewList';
+import ProductReviewStats from './review/ProductReviewStats';
+import ProductData from './data/ProductData';
+import ReviewForm from './review/ReviewForm';
 
 function App() {
+  const [review, setReview] = useState(ProductData);
+  const addReview = newReview => {
+    newReview.id = uuidv4();
+    setReview([newReview, ...review]);
+  };
+  const deleteFeedback = id => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setReview(review.filter(item => item.id !== id));
+    }
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Header />
+      <div className="container">
+        <ReviewForm handleAdd={addReview} />
+        <ProductReviewStats review={review} />
+        <ProductReviewList 
+        review={review} 
+        handleDelete={deleteFeedback} 
+  />
 
+      </div>
+   </>
+ );
+}
 export default App;
---
+++ src/data/ProductData.js
@@ -0,0 +1,18 @@
+const ProductData = [
+    {
+      id: 1,
+      rating: 5,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.    consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.'
+    },
+    {
+     id: 2,
+     rating: 4,
+     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.'
+    },
+    {
+    id: 3,
+    rating: 4,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.'
+    }
+  ];
+  export default ProductData;
